baddbe9c2a16b09b3187e594b13c5d9aca7de649
AgnesDevita <agnesdevitaa01@gmail.com>
2026-01-06 23:19:34 +0700

    feat: update Next.js configuration to enable turbopack support

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-	/* config options here */
-};
-
-export default nextConfig;
-
 // Enable calling `getCloudflareContext()` in `next dev`.
 // See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
+const nextConfig: NextConfig = {
+	turbopack: {
+		root: __dirname,
+	},
+};
+
 initOpenNextCloudflareForDev();
+
+export default nextConfig;
